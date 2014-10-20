123f4a1d8e67e418768af44a10adb3519a1d4185
tiny <tzhang920314@gmail.com>
2014-10-20 14:54:36 +0800

    单独实现了过滤商品的功能

--- test/spec/FilterSpec.js
+++ test/spec/FilterSpec.js
@@ -2,9 +2,10 @@
  * Created by tiny on 14-10-17.
  */
 describe("cart_items filter test", function() {
-    it("should give us string without backspace", function() {
+    it("should return the filtered items 苹果 iMac", function() {
+        
         var t = filter_the_items(Promotion_rule.get_rule(),Cart_items.load_cart_items());
-//        expect(str).toBe("(name=='苹果'||name=='iMac')&&publish_time<'08/01/2014'");
-        console.log(t[0]);
+        expect(t[0].properties[1].property_value).toBe("苹果");
+        expect(t[1].properties[1].property_value).toBe("iMac");
     })
 });
